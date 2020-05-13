f7986f23cac3f6e7bf3de2afa81cd9a0f78fab70
David Garcia Latorre <dgarcia@seocom.agency>
2020-05-13 08:54:05 +0200

    Added code to be able to play with the response time when downloading the elements of the page.

---
+++ proxy.php
@@ -0,0 +1,44 @@
+<?php
+$uri = __DIR__ . $_SERVER['REQUEST_URI'];
+if ($_SERVER['REQUEST_URI'] == '/') {
+    $uri .= 'index.html';
+}
+
+
+$mime = custom_mime_content_type($uri);
+$size = filesize($uri);
+
+$delay = 0;
+
+header(sprintf('Content-Type: %s', $mime));
+header('Expires: 0');
+header('Cache-Control: must-revalidate');
+header('Pragma: public');
+header(sprintf('Content-Length: %d', $size));
+header(sprintf('X-TTFB-Delay: %d secs', $delay ) );
+
+if ( $delay>0 ) {
+    sleep($delay);
+}
+
+readfile($uri);
+
+
+function custom_mime_content_type($filename)
+{
+
+    $mime_types = array(
+        'htm' => 'text/html',
+        'html' => 'text/html',
+        'css' => 'text/css',
+        'jpg' => 'image/jpeg',
+        'ico' => 'image/vnd.microsoft.icon',
+    );
+
+    $ext = strtolower(array_pop(explode('.', $filename)));
+    if (array_key_exists($ext, $mime_types)) {
+        return $mime_types[$ext];
+    } else {
+        return 'application/octet-stream';
+    }
+}
